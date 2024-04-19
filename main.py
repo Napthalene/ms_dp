d786196f85f88cb7d12227b97cdddfbdce1010af
Adam <Napthalene@github.com>
2024-04-19 12:23:04 +0200

    zmena nacitavania dat (nacitavaju sa vsetky naraz do ram ako dictionary a potom sa z nich nahodne vyberaju datasety do epoch; zmena trenovacieho procesu aby sedel na nacitanie dat; dynamicky learning rate sa stale pocita na zaklade loss na trenovacich datach

--- main.py
+++ main.py
@@ -21,92 +21,132 @@ else:
     print("Pouzitie CPU.")
 
 
+def data_preload(start_data, end_data, reduced, priecinok_cesta=f"{os.getcwd()}\\data"):
+    datasets = dict()
+    # Allokuje priestor pre FRF
+    frf_data_len = np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{1}.csv", delimiter=',', skip_header=0)[0,
+                   :].__len__() if not reduced else np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{1}.csv", delimiter=',',
+                                                                  skip_header=0)[0, ::2].__len__()
+    frf_data = np.zeros([1, frf_data_len * 2])
+
+    vuf_data = np.zeros([1, 2])
+    damp_data = np.zeros([1, 2])
+
+    # Vytvori dictionary so vsetkymi datami s priradenym indexovanim
+    for i, data_index in enumerate(range(start_data, end_data + 1)):
+        temp_dict = dict()
+        temp_dict["data_index"] = data_index
+        # Dictionary FRF
+        frf_dataset = np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{data_index}.csv", delimiter=',',
+                                    skip_header=0) if not reduced else np.genfromtxt(
+            f"{priecinok_cesta}/FRF_databaza/FRF_{data_index}.csv", delimiter=',', skip_header=0)[:, ::2]
+        frf_data[0, :frf_data_len] = frf_dataset[0, :]
+        frf_data[0, frf_data_len:] = np.log(frf_dataset[1, :])  # Logaritmovanie y hodnoty
+        temp_dict["frf"] = frf_data
+
+        # Dictionary VUF
+        vuf_data[0, :] = np.genfromtxt(f"{priecinok_cesta}/VUT_&_pomerne_tlmenie/vut_pomerne_tlmenie_{data_index}.csv", delimiter=',',
+                                       skip_header=0)[0, :]
+        temp_dict["vuf"] = vuf_data
+
+        # Dictionary Tlmenie
+        damp_data[0, :] = np.genfromtxt(f"{priecinok_cesta}/VUT_&_pomerne_tlmenie/vut_pomerne_tlmenie_{data_index}.csv", delimiter=',',
+                                        skip_header=0)[1, :]
+        temp_dict["tlmenie"] = damp_data
+
+        datasets[f"{i}"] = temp_dict
+    return datasets
+
 # Nacitavanie random input datasetov
-def load_random_datasets(random_data_indexes, reduced=False ,priecinok_cesta=f"{os.getcwd()}\\data"):
-    dataset = np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{1}.csv", delimiter=',', skip_header=0) if not reduced else np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{1}.csv", delimiter=',', skip_header=0)[:, ::2]
-    data_len = dataset[0, :].__len__()
-    data_num = random_data_indexes.shape[1]
-    datasets = np.zeros([data_num, 1, data_len*2])
-    # nacitavanie_string = "Nacitavanie inputov"
-    # print(nacitavanie_string, end="")
-    for index, random_data_index in enumerate(random_data_indexes[0, :]):
-        # if index % 10 == 0:
-        #     if nacitavanie_string == "Nacitavanie inputov...":
-        #         nacitavanie_string = "Nacitavanie inputov"
-        #     else:
-        #         nacitavanie_string += "."
-        # print(f"\r{index+1}/{data_num} {nacitavanie_string}", end="")
-        dataset = np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{random_data_index}.csv", delimiter=',', skip_header=0) if not reduced else np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{random_data_index}.csv", delimiter=',', skip_header=0)[:, ::2]
-        datasets[index, 0, :data_len] = dataset[0, :]
-        datasets[index, 0, data_len:] = np.log(dataset[1, :])  # Logaritmovanie y hodnoty
-    # print(f"\r{index+1}/{data_num} Inputy nacitane")
-    return datasets
-
-
-# Nacitanie random verification datasetov
-def load_random_verification_dataset(random_data_indexes, priecinok_cesta=f"{os.getcwd()}\\data"):
-    dataset = np.genfromtxt(f"{priecinok_cesta}/VUT_&_pomerne_tlmenie/vut_pomerne_tlmenie_{1}.csv", delimiter=',', skip_header=0)
-    data_len = dataset[0, :].__len__()
-    data_num = random_data_indexes.shape[1]
-    datasets = np.zeros([data_num, 2, data_len])
-    # nacitavanie_string = "Nacitavanie outputov"
-    # print(nacitavanie_string, end="")
-    for index, random_data_index in enumerate(random_data_indexes[0, :]):
-        # if index % 10 == 0:
-        #   if nacitavanie_string == "Nacitavanie outputov...":
-        #       nacitavanie_string = "Nacitavanie outputov"
-        #   else:
-        #       nacitavanie_string += "."
-        # print(f"\r{index+1}/{data_num} {nacitavanie_string}", end="")
-        dataset = np.genfromtxt(f"{priecinok_cesta}/VUT_&_pomerne_tlmenie/vut_pomerne_tlmenie_{random_data_index}.csv", delimiter=',', skip_header=0)
-        datasets[index, 0, :] = dataset[0, :]
-        datasets[index, 1, :] = dataset[1, :]
-    # print(f"\r{index+1}/{data_num} Overovacie outputy nacitane")
-    return datasets
+# def load_random_datasets(random_data_indexes, reduced=False ,priecinok_cesta=f"{os.getcwd()}\\data"):
+#     dataset = np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{1}.csv", delimiter=',', skip_header=0) if not reduced else np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{1}.csv", delimiter=',', skip_header=0)[:, ::2]
+#     data_len = dataset[0, :].__len__()
+#     data_num = random_data_indexes.shape[1]
+#     datasets = np.zeros([data_num, 1, data_len*2])
+#     # nacitavanie_string = "Nacitavanie inputov"
+#     # print(nacitavanie_string, end="")
+#     for index, random_data_index in enumerate(random_data_indexes[0, :]):
+#         # if index % 10 == 0:
+#         #     if nacitavanie_string == "Nacitavanie inputov...":
+#         #         nacitavanie_string = "Nacitavanie inputov"
+#         #     else:
+#         #         nacitavanie_string += "."
+#         # print(f"\r{index+1}/{data_num} {nacitavanie_string}", end="")
+#         dataset = np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{random_data_index}.csv", delimiter=',', skip_header=0) if not reduced else np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{random_data_index}.csv", delimiter=',', skip_header=0)[:, ::2]
+#         datasets[index, 0, :data_len] = dataset[0, :]
+#         datasets[index, 0, data_len:] = np.log(dataset[1, :])  # Logaritmovanie y hodnoty
+#     # print(f"\r{index+1}/{data_num} Inputy nacitane")
+#     return datasets
+#
+#
+# # Nacitanie random verification datasetov
+# def load_random_verification_dataset(random_data_indexes, priecinok_cesta=f"{os.getcwd()}\\data"):
+#     dataset = np.genfromtxt(f"{priecinok_cesta}/VUT_&_pomerne_tlmenie/vut_pomerne_tlmenie_{1}.csv", delimiter=',', skip_header=0)
+#     data_len = dataset[0, :].__len__()
+#     data_num = random_data_indexes.shape[1]
+#     datasets = np.zeros([data_num, 2, data_len])
+#     # nacitavanie_string = "Nacitavanie outputov"
+#     # print(nacitavanie_string, end="")
+#     for index, random_data_index in enumerate(random_data_indexes[0, :]):
+#         # if index % 10 == 0:
+#         #   if nacitavanie_string == "Nacitavanie outputov...":
+#         #       nacitavanie_string = "Nacitavanie outputov"
+#         #   else:
+#         #       nacitavanie_string += "."
+#         # print(f"\r{index+1}/{data_num} {nacitavanie_string}", end="")
+#         dataset = np.genfromtxt(f"{priecinok_cesta}/VUT_&_pomerne_tlmenie/vut_pomerne_tlmenie_{random_data_index}.csv", delimiter=',', skip_header=0)
+#         datasets[index, 0, :] = dataset[0, :]
+#         datasets[index, 1, :] = dataset[1, :]
+#     # print(f"\r{index+1}/{data_num} Overovacie outputy nacitane")
+#     return datasets
 
 
 # Nacitanie dat na verifikaciu
-def load_verification_datasets(data_index, reduced=False, priecinok_cesta=f"{os.getcwd()}\\data"):
-    input_dataset = np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{data_index}.csv", delimiter=',', skip_header=0) if not reduced else np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{data_index}.csv", delimiter=',', skip_header=0)[:, ::2]
-    data_len = input_dataset[0, :].__len__()
-    verification_input = np.zeros([1, 1, data_len*2])
-    verification_input[0, 0, :data_len] = input_dataset[0, :]
-    verification_input[0, 0, data_len:] = np.log(input_dataset[1, :])
-    output_dataset = np.genfromtxt(f"{priecinok_cesta}/VUT_&_pomerne_tlmenie/vut_pomerne_tlmenie_{data_index}.csv", delimiter=',', skip_header=0)
-    data_len = output_dataset[0, :].__len__()
-    verification_output = np.zeros([1, 2, data_len])
-    verification_output[0, 0, :] = output_dataset[0, :]
-    verification_output[0, 1, :] = output_dataset[1, :]
-    return verification_input, verification_output
+# def load_verification_datasets(data_index, reduced=False, priecinok_cesta=f"{os.getcwd()}\\data"):
+#     input_dataset = np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{data_index}.csv", delimiter=',', skip_header=0) if not reduced else np.genfromtxt(f"{priecinok_cesta}/FRF_databaza/FRF_{data_index}.csv", delimiter=',', skip_header=0)[:, ::2]
+#     data_len = input_dataset[0, :].__len__()
+#     verification_input = np.zeros([1, 1, data_len*2])
+#     verification_input[0, 0, :data_len] = input_dataset[0, :]
+#     verification_input[0, 0, data_len:] = np.log(input_dataset[1, :])
+#     output_dataset = np.genfromtxt(f"{priecinok_cesta}/VUT_&_pomerne_tlmenie/vut_pomerne_tlmenie_{data_index}.csv", delimiter=',', skip_header=0)
+#     data_len = output_dataset[0, :].__len__()
+#     verification_output = np.zeros([1, 2, data_len])
+#     verification_output[0, 0, :] = output_dataset[0, :]
+#     verification_output[0, 1, :] = output_dataset[1, :]
+#     return verification_input, verification_output
 
 
 def accuracy_test(output, verification):
     output = output.cpu().detach().numpy()
     verification = verification.cpu().detach().numpy()
-    out_1_acuraccy = (1 - abs(output[0] - verification[0]) / verification[0]) * 100
-    out_2_acuraccy = (1 - abs(output[1] - verification[1]) / verification[1]) * 100
+    out_1_acuraccy = (1 - abs(output[0, 0] - verification[0, 0]) / verification[0, 0]) * 100
+    out_2_acuraccy = (1 - abs(output[0, 1] - verification[0, 1]) / verification[0, 1]) * 100
+    # print("\nout_1", output[0,0], "out_2", output[0,1])
+    # print("ver_1", verification[0,0], "ver_2", verification[0, 1])
+    # print("1_accuracy", out_1_acuraccy)
+    # print("2_accuracy", out_2_acuraccy)
     return (out_1_acuraccy+out_2_acuraccy)/2
 
 
 # Vytvaranie nahodnych batch-ov datasetov
-def create_random_choice(num_of_data_in_batch, num_of_datasets):
-    mylist = np.linspace(1, num_of_datasets, num_of_datasets, dtype=int).tolist()
-    i = 0
-    num_of_batches = num_of_datasets//num_of_data_in_batch
-    random_datasets = np.zeros([num_of_batches, 1, num_of_data_in_batch], dtype=int)
-    dataset_index = 0
-    while True:
-        temp = random.choice(mylist)
-        mylist.remove(temp)
-        # print(f"{i} temp:", temp)
-        random_datasets[dataset_index, 0, i] = temp
-        i += 1
-        if i >= num_of_data_in_batch:
-            dataset_index += 1
-            i = 0
-        if dataset_index >= num_of_batches:
-            break
-    return random_datasets
+# def create_random_choice(num_of_data_in_batch, num_of_datasets):
+#     mylist = np.linspace(1, num_of_datasets, num_of_datasets, dtype=int).tolist()
+#     i = 0
+#     num_of_batches = num_of_datasets//num_of_data_in_batch
+#     random_datasets = np.zeros([num_of_batches, 1, num_of_data_in_batch], dtype=int)
+#     dataset_index = 0
+#     while True:
+#         temp = random.choice(mylist)
+#         mylist.remove(temp)
+#         # print(f"{i} temp:", temp)
+#         random_datasets[dataset_index, 0, i] = temp
+#         i += 1
+#         if i >= num_of_data_in_batch:
+#             dataset_index += 1
+#             i = 0
+#         if dataset_index >= num_of_batches:
+#             break
+#     return random_datasets
 
 
 # Fukcia na vypocet casu
@@ -146,22 +186,22 @@ priecinok_cesta = f"{os.getcwd()}\\data"
 # end_data    -> cislo POSLEDNYCH dat intervalu na ktorom sa bude trenovat
 # num_of_data_in_batch -> pocet nahodnych datasetov vchadzjucich do trenovacieho procesu
 start_data, end_data = (1, 8000)
-num_of_data_in_batch = 8000
-epochs = 100                        # Pocet epoch
+ver_start_data, ver_end_data = (8001, 10000)
+epochs = 50                        # Pocet epoch
 reduced = True                      # Zredukuje vstupne data na polovicu a tym sa zmensi aj pocet neur. v input vrstve
-average_loss_accuracy_num = 1000               # pocet iteracii na ulozenie loss
+average_loss_accuracy_num = 10               # pocet iteracii na ulozenie loss
+output_option = "vuf"   # "tlmenie"
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
 
 print(f"Nacitane - pocet trenovacich datasetov: {end_data}\n\
-         - pocet datasetov v batch-i:   {num_of_data_in_batch}\n\
-         - pocet epoch:                 {epochs}"\
-      if (end_data % num_of_data_in_batch == 0) or end_data < num_of_data_in_batch else\
-      "Zadane hodnoty nie su spravne")
+         - Trenovanie:                  {output_option}\n\
+         - pocet epoch:                 {epochs}\n\
+         - Redukcia dat:                {'Ano' if reduced else 'Nie'}\n")
 
 # Vyber poctu neuronov input vrstvy
-example_input = load_random_datasets(np.array([[1]]), reduced)
-n_input_layers = example_input.shape[2]
+example_input = data_preload(1,1,reduced)
+n_input_layers = example_input["0"]["frf"].shape[1]
 n_output_layers = 2
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -171,7 +211,7 @@ learning_rate = 0.001              # Learning rate
 h1, h2, h3, h4 = 624, 312, 156, 78          # Velkost jednotlivych vrstiev v modeli NN
 layers_num = 5                      # Celkovy pocet vrstiev
 # Nastavenie ukladania
-model_hyperparameters = f"model_{str(learning_rate).replace('.', 'dot')}lr_{epochs}e_{num_of_data_in_batch}b_{end_data}d"
+model_hyperparameters = f"model_{str(learning_rate).replace('.', 'dot')}lr_{epochs}e_{end_data}d"
 model_parameters = f"model_{model_type}_{layers_num}l_{n_input_layers}il_{h1}_{h2}_{h3}_{h4}_{n_output_layers}ol"
 model_name = "model.pth"
 model = VUF_odhadovac(n_input_layers, n_output_layers, h1, h2, h3, h4).to(device) # Inicializacia modelu
@@ -206,48 +246,66 @@ temp_accuracy = 0.0
 epoch_loss = 0.0
 model.train()
 
+print("Nacitavanie dat...")
+preload_data = data_preload(start_data, end_data, True)
+print("Data Nacitane!\nTrenovanie:\n")
+shuffle_indexes = list(range(end_data-start_data))
+
 for epoch in range(epochs):
+    # Zapnutie casovaca jednej epochy
     timer_start = time.time()
-    # Nahodne rozhadzanie dat do batch-ov
-    batches = create_random_choice(num_of_data_in_batch, end_data)
-    # Nacitavanie jednotlivych batch-ov
-    for batch_index, batch in enumerate(batches):
-        print(f"\rEpocha: {epoch+1}/{epochs}, Batch: {batch_index+1}/{batches.shape[0]}, Zostava: {iterations_left} iter, Odhadovany cas vypoctu: {hod}h {min}m {sec}s\t", end="")
-        input_datasets = load_random_datasets(batch, reduced)
-        verification_datasets = load_random_verification_dataset(batch)
 
-        # Trenovanie nacitaneho batchu
-        # print("Trenovanie:")
-        for index, dataset in enumerate(input_datasets):
-            input_data = torch.from_numpy(dataset[0, :]).type(torch.float32).to(device)
-            verification_data = torch.from_numpy(verification_datasets[index, 0, :]).type(torch.float32).to(device)
-            outputs = model(input_data)
-            loss = criterion(outputs, verification_data)
+    # Vytvorenie rozhadzania indexov dat
+    random.shuffle(shuffle_indexes)
 
-            optimizer.zero_grad() # Vynulovanie gradientu
-            loss.backward()
-            optimizer.step()
+    # Trenovaci cyklus na rozhadzanych datach
+    for i, data_index in enumerate(shuffle_indexes):
+        # Nacitanie inputov a porovnavacich dat
+        input_data = torch.from_numpy(preload_data[f"{data_index}"]["frf"]).type(torch.float32).to(device)
+        sim_output = torch.from_numpy(preload_data[f"{data_index}"][output_option]).type(torch.float32).to(device)
+        outputs = model(input_data)
+        loss = criterion(outputs, sim_output)
 
-            # Zapisanie do tensorboardu
-            iterations_left -= 1
-            # writer.add_scalar("loss", loss, total_iterations - iterations_left)
+        optimizer.zero_grad() # Vynulovanie gradientu
+        loss.backward()
+        optimizer.step()
 
-            temp_loss += loss.item()
-            epoch_loss += loss.item()
-            temp_accuracy += accuracy_test(outputs, verification_data)
-            if (total_iterations - iterations_left) % average_loss_accuracy_num == 0:
-                loss_list.append(temp_loss / average_loss_accuracy_num)
-                accuracy_list.append(temp_accuracy / average_loss_accuracy_num)
-                temp_loss = 0.0
-                temp_accuracy = 0.0
+        # Zapisanie do tensorboardu
+        iterations_left -= 1
+        # writer.add_scalar("loss", loss, total_iterations - iterations_left)
 
-            print(f"\rEpocha: {epoch+1}/{epochs}, Batch: {batch_index+1}/{batches.shape[0]}, Zostava: {iterations_left} iter, Odhadovany cas vypoctu: {hod}h {min}m {sec}s\t", end="")
+        temp_loss += loss.item()
+        epoch_loss += loss.item()
+        temp_accuracy += accuracy_test(outputs, sim_output)
+        if (total_iterations - iterations_left) % average_loss_accuracy_num == 0:
+            loss_list.append(temp_loss / average_loss_accuracy_num)
+            accuracy_list.append(temp_accuracy / average_loss_accuracy_num)
+            temp_loss = 0.0
+            temp_accuracy = 0.0
+
+        print(f"\rEpocha: {epoch+1}/{epochs}, Zostava: {iterations_left} iter, Odhadovany cas vypoctu: {hod}h {min}m {sec}s\t", end="")
     scheduler.step(epoch_loss / end_data)
     epoch_loss = 0.0
     # writer.close()
     timer_epoch.append(time.time() - timer_start)
     hod, min, sec = calc_time((epochs-(epoch+1))*np.mean(timer_epoch))
-print("\n\nTrenovanie dokoncene!")
+print("\n\nTrenovanie dokoncene!\n\n")
+
+# Vypocet accuracy na datach na ktorych nebol model trenovany
+print("Vypocet accuracy na verifikacnych datach:")
+preload_verification_data = data_preload(ver_start_data, ver_end_data, reduced)
+ver_data_num = ver_end_data - ver_start_data
+ver_accuracy = 0.0
+model.eval()
+with torch.no_grad():
+    for i in range(ver_data_num):
+        ver_input = torch.from_numpy(preload_verification_data[f"{i}"]["frf"]).type(torch.float32).to(device)
+        ver_sim_output = torch.from_numpy(preload_verification_data[f"{i}"][output_option]).type(torch.float32).to(device)
+        ver_output = model(ver_input)
+        ver_accuracy += accuracy_test(ver_output, ver_sim_output)
+
+print(f"\nPresnost modelu na novych datach: {round(ver_accuracy / ver_data_num, 2)}%")
+print('Oslava??')
 
 # Create a figure and axis object
 fig, ax = plt.subplots()
